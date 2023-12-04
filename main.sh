68185010b5521f1baf630a4d1a8a1ac4fea05af0
Ubuntu <comp141@ML-RefVm-949980.zv0ixxhdtw1epaxq4zps5kvxsc.ex.internal.cloudapp.net>
2023-12-04 07:19:56 +0000

    Second commit

--- main.sh
+++ main.sh
@@ -1,2 +1,22 @@
 chmod +x main.sh
+assignment_directory="extra-credit"
 
+output_directory="output"
+mkdir "$output_directory"
+echo "'$output_directory' made."
+
+mv extracredit.txt "$output_directory/"
+echo "extracredit.txt moved to '$output_directory' directory"
+
+cd "$output_directory"
+echo "Directory changed to '$output_directory'."
+
+cat extracredit.txt > read.txt
+
+pwd > pwd.txt
+
+ls > ls.txt 
+
+alias today="date '+%Y-%m-%d'"
+
+today > date.txt
